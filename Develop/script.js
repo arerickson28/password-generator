cd38a84781fdc7bed51f28fb1010859e793435b1
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-04-29 13:56:42 -0500

    updated readme and added comments in js code

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,13 @@
-// ## Acceptance Criteria
-
-// ```
-// GIVEN I need a new, secure password
-
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// ```
-
-
 // ------------------------------------------------------------
 // Begin Starter Code
 // ------------------------------------------------------------
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -46,101 +18,176 @@ generateBtn.addEventListener("click", writePassword);
 // ------------------------------------------------------------
 // End Starter Code
 // ------------------------------------------------------------
-// ------------------------------------------------------------
-// My Code Start
+// Begin My Code
 // ------------------------------------------------------------
 
-// Present Prompts
+//Main funtion will fetch input for prompt1, then fetch input for prompt2, then use inputs to make the password
 function generatePassword() {
-  //prompt for character length
-  console.log("sending to charLen") ;
-  let prompt1 = charLen() ;
-  console.log(prompt1 + " is the password") ;
- 
-  //prompt for special characters
-  // let prompt2 = specialChar() ;
-  //funtion to generate password
-
-  console.log("printing password: " + prompt1) ;
-  passWord = prompt1 ;
-  return passWord
+    let prompt1 = getPrompt1() ;
+    let prompt2 = getPrompt2() ;
+    let passWord = makePassword(prompt1, prompt2) ;
+    return passWord
 }
-// ------------------------------------------------------------
-// Function to choose character length
-// ------------------------------------------------------------
-function charLen() {
-  // console.log("charLen triggered") ;
-  let chosenLen = parseInt(prompt("Choose character length of password", "Enter a number between 8 and 128")) ;
-  
-  console.log("Entered: " + chosenLen) ;
-  console.log("sending " + chosenLen + " to checkValidNum") ;
-  // validate legitamite number
-  let legitNum = checkValidNum(chosenLen) ;
-  console.log(legitNum + " is a number") ;
-  // validate length
-  console.log("sending " + legitNum + " to isLongEnough") ;
-  let longEnough = isLongEnough(legitNum) ;
-  console.log(longEnough + " is appropriate length") ;
-  console.log("returning " + longEnough + " to generatePassword") ;
 
-  return longEnough
+
+//Function to Get Prompt11
+function getPrompt1() {
+    let charLen = parseInt(prompt("Please choose password length between 8 and 128" + "\n" + "Enter as digit")) ;
+
+    console.log(typeof(charLen)) ;
+
+    //Validating character length and that entry isn't a word
+   if (charLen >= 8 && charLen <= 128) {
+       console.log(charLen + " is acceptable length") ;
+   } else {
+       alert("Please choose an appropriate length") ;
+       return getPrompt1()
+   }
+
+   console.log("prompt1 has returned " + charLen)
+   return charLen
 }
-// ------------------------------------------------------------
-// Function to validate if chosen character length is a number and not letters
-// ------------------------------------------------------------
-function checkValidNum(entry) {
-  console.log(entry + " is NaN?: " + isNaN(entry)) ;
-  if (isNaN(entry) === false ) {
+
+
+// Function to get prompt2
+function getPrompt2() {
     
-    console.log(entry + " is acceptable") ;
-    console.log("returning " + entry + " to charLen") ;
-    return entry
-    // console.log("sending entry to isLongEnough") ;
-    // validEntry = isLongEnough(entry) ;
-    // return validEntry 
+    //Array to hold user choices
+    let chosenCategories = [] ;
 
-  } else {
-    console.log(entry + " not acceptable") ;
-    console.log("discarding " + entry) ;
-    console.log("sending to returnToCharLen") ;
-    returnTocharLen() ;
-  }
+    let upperCase = confirm("Would you like to use uppercase?" + "\n" + "Select 'okay' for yes and 'cancel' for no") ;
+
+    //If user selects 'okay', add character cattegory to the chosenCategories array
+    if (upperCase === true) {
+        upperCase = "upperCase"
+        chosenCategories.push(upperCase) ;
+        console.log(chosenCategories) ;
+    }    
+
+    let lowerCase = confirm("Would you like to use lowercase?" + "\n" + "Select 'okay' for yes and 'cancel' for no" ) ;
+
+    if (lowerCase === true) {
+        lowerCase = "lowerCase"
+        chosenCategories.push(lowerCase) ;
+        console.log(chosenCategories) ;
+    }  
+
+    let special = confirm("Would you like to use special characters?"  + "\n" + "Select 'okay' for yes and 'cancel' for no") ;
+
+    if (special === true) {
+        special = "special"
+        chosenCategories.push(special) ;
+        console.log(chosenCategories) ;
+    }  
+
+    let numbers = confirm("Would you like to use numbers?"  + "\n" + "Select 'okay' for yes and 'cancel' for no") ;
+
+    if (numbers === true) {
+        numbers = "numbers"
+        chosenCategories.push(numbers) ;
+        console.log(chosenCategories) ;
+    }  
+
+    //verify chosenCategories.length is at least one
+    if (chosenCategories.length === 0) {
+        alert("Please select at least one character type") ;
+        getPrompt2() ;
+    }
+
+    console.log("prompt2 is returning: " + chosenCategories) ;
+    return chosenCategories
+    
 }
-  // ------------------------------------------------------------
-  // Function to validate if chosen character length is between 8 and 128 characters
-  // ------------------------------------------------------------
-function isLongEnough(charLen) {
-  console.log("is long enough triggered") ;
-  if (charLen >= 8 && charLen <= 128) {
-    console.log(charLen + " is acceptable") ;
-    console.log("returning " + charLen + " to charLen")
-    // console.log("returning " + charLen + " to checkValidNum") ;
-    return charLen
-  } else {
-    console.log("not acceptable: " + charLen + " is either to short or too long") ;
-    console.log("send to returnTocharLen") ;
-    returnTocharLen()
-  }
+
+//Character arrays available for making password
+let lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] ;
+
+let upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] ;
+
+let numericalArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"] ;
+
+let specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "?"] ;
+
+
+
+//Function to use prompts to make password
+function makePassword(prompt1, prompt2) {
+    //The arrays that will be used in makeing this password
+    let charArraysToInclude = [] ;
+    //Arrays to be used will be joined into one array, finalPasswordArray
+    let finalPasswordArray = [] ;
+    //After the random selection, finalPassword will hold the characters for the final password
+    let finalPassword = [] ;
+    
+    //If statements to gather desired arrays
+    if (prompt2.includes("upperCase")) {
+        charArraysToInclude.push(upperCaseArray) ;
+    } 
+    
+    if (prompt2.includes("lowerCase")) {
+        charArraysToInclude.push(lowerCaseArray) ;
+    } 
+    
+    if (prompt2.includes("special")) {
+        charArraysToInclude.push(specialArray) ;
+    }
+    
+    if (prompt2.includes("numbers")){
+        charArraysToInclude.push(numericalArray) ;
+    }
+
+    console.log("the charArraysToInclude are: " + charArraysToInclude)
+    console.log("length of charArrays to include: " + charArraysToInclude.length) ;
+
+    //To ensure that at least one character from each desired character category is included, we pick these first
+    for (var i=0; i < charArraysToInclude.length; i++) {
+        let randomNumber = Math.floor(Math.random() * charArraysToInclude[i].length) ;
+        finalPassword.push(charArraysToInclude[i][randomNumber]) ;
+    }
+
+    console.log("Final Password thus far: " + finalPassword)
+
+    //After adding one character for each cattegory, we need to calculate the remaining characters to be added
+    let charToBeAdded = prompt1 - charArraysToInclude.length ;
+
+    //For loop to combine the included haracter arrays into one big array
+    for (var i = 0; i < charArraysToInclude.length; i++) {
+        finalPasswordArray = finalPasswordArray.concat(charArraysToInclude[i]) ;
+    }
+    
+    console.log("finalPasswordArray is: " + finalPasswordArray)
+
+    //Randomly adding the remaining characters to the finalPassword
+    for (var i=0; i < charToBeAdded; i++){
+        let randomNumber = Math.floor(Math.random() * finalPasswordArray.length) ;
+        finalPassword.push(finalPasswordArray[randomNumber])
+    }
+
+    console.log("finalPassword is: " + finalPassword)
+
+    //Shuffle password for total randomness
+    shufflePassword(finalPassword) ;
+
+    console.log("shuffled password: " + finalPassword) ;
+
+    //Convert final password from an array to one long string without commas
+    finalPassword = finalPassword.join('') ;
+
+    console.log("joined final password is: " + finalPassword) ;
+
+    return finalPassword
+
 }
 
-// ------------------------------------------------------------
-// Function to give alert to re-enter value as a number between 8 and 128 (no letters)
-// ------------------------------------------------------------
-function returnTocharLen() {
-  alert("Error: Entry needs to be a numeric value between 8 and 128. Try again.") ;
-  console.log("returning to charLen") ;
-  charLen()
+//Durstenfeld Shuffle, a form of the Fisher-Yates shuffle
+
+//Works by looping backwards through the array and randomly
+//replacing last index with a lower index until all indexes have been replaced
+function shufflePassword(array) {
+    for (var i = array.length -1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i+1)) ;
+        var temp = array[i] ;
+        array[i] = array[j] ;
+        array[j] = temp ;
+    }
 }
-// ------------------------------------------------------------
-
-
-
-
-
-
-
-
-// ------------------------------------------------------------
-// ------------------------------------------------------------
-// My Code End
-// ------------------------------------------------------------
--- README.md
+++ README.md
@@ -1,32 +1,11 @@
 # password-generator
-Web Dev JS assignment
+### Web Dev JavaScript assignment
+---
+## This repository contains a password generator that will generate a random string of characters of specified length and character type  
 
+* Instructions for the assignment can be found in the instructions folder
+* Code submission can be found in the Develop folder
+---
 
-# Prompts 1 for character length
--convert entry from string type to number type (NaN is considered a number)
-* validate entry type is a number
-    * if statement to check this
-        * yes, great...what now?
-        * no, try again...how?
-
-* validate entry is between 8 and 128 
-    * if statement to check this
-        * yes, great...what now?
-        * no, try again...how?
-
-
-
-# Prompt 2 for special characters
---Option, a yes/no prompt for each: upper, lower, number, special (convert response to lower)  
---Option, one prompt then use "check if contains" upper, lower, number, special (convert response to lower) 
---Convert to boolean 
---Append answer to a list (four values in list, yes/no for each of the four choices)  
-
-
-# Use information gathered from prompt 1 & 2 to generate password
-* Create four lists, one for upppercase letters, lowercase letters, digits, and special
-* Select character cattegories to be included 
-* validate that at least one category of characters has been included
-* Use password length to randomize the number of occurrences to allocate to each category
-* Each category uses the allocated number for the number of itterations of randomly selecting charaters from their respective list. Each time appending the character to a master list.
-* Finally, shuffle master list.
+*image of password generator*
+![Password Generator](password_generator.png "image of password generator")
